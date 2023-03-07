18117fc4cf23bc7469abd28de4381d9f82ebbb1c
mobest1an <erikk202@gmail.com>
2023-03-07 18:12:22 +0300

    Update tests

--- src/main/java/tpo/ArcSinTaylor.java
+++ src/main/java/tpo/ArcSinTaylor.java
@@ -3,14 +3,20 @@ package tpo;
 public class ArcSinTaylor {
 
     public static double calculateArcSin(double x, double eps) {
+        if (x < -1 || x > 1) {
+            throw new IllegalArgumentException("x cannot be less than 1 or greater than 1");
+        }
+
         double result = 0;
         double term = x;
         int i = 1;
+
         while (Math.abs(term) > eps) {
             result += term;
             term *= ((double) (2 * i - 1) * x * x) / ((double) 2 * i * (2 * i + 1));
             i++;
         }
+
         return result;
     }
 }
--- src/test/java/L3/tpo/ArcSinTaylorTests.java
+++ src/test/java/L3/tpo/ArcSinTaylorTests.java
@@ -5,16 +5,37 @@ import org.junit.jupiter.params.provider.CsvSource;
 import tpo.ArcSinTaylor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ArcSinTaylorTests {
 
     @ParameterizedTest
     @CsvSource({
             "-0.3,1e-6,-0.304693",
+            "0,1e-6,0",
             "0.3,1e-6,0.304693",
             "0.5,1e-6,0.523599",
     })
-    public void testArcSinTaylor(double x, double eps, double expected) {
+    public void testCommonArguments(double x, double eps, double expected) {
         assertEquals(expected, ArcSinTaylor.calculateArcSin(x, eps), 0.01);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-1.001,1e-6",
+            "100,1e-6",
+            "1.001,1e-6",
+    })
+    public void testIllegalArguments(double x, double eps) {
+        assertThrows(IllegalArgumentException.class, () -> ArcSinTaylor.calculateArcSin(x, eps));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0.9999,1e-9,1.55665",
+            "-0.9999,1e-9,-1.55665",
+    })
+    public void testBorderlineArguments(double x, double eps, double expected) {
+        assertEquals(expected, ArcSinTaylor.calculateArcSin(x, eps), 0.5);
     }
 }
